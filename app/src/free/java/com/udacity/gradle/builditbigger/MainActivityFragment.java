62fe28479de8df45e4ca8f5bfd755b4f604c1cd5
tania <tincio.gomez@gmail.com>
2017-04-13 21:36:54 -0500

    add productFlavors

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,21 +1,30 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
+import com.example.JokeLib;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+import com.tincio.example.jokeandroidlib.ShowJokeActivity;
+import com.udacity.gradle.builditbigger.service.EndpointsAsyncTask;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener  {
+
+    ProgressBar progressBar;
+    InterstitialAd mInterstitialAd;
+    Button btnShowJoke;
 
     public MainActivityFragment() {
     }
@@ -24,16 +33,65 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
-
+        progressBar = (ProgressBar)root.findViewById(R.id.progressBar);
+        this.btnShowJoke = (Button)root.findViewById(R.id.btn_show_joke);
+        this.btnShowJoke.setOnClickListener(this);
+        //add anuncion intersticiales
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
+        requestNewInterstitial();
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                showJoke();
+            }
+        });
         return root;
     }
+
+    void activeButtonJoke(Boolean active){
+        this.btnShowJoke.setEnabled(active);
+    }
+    //obtener jokes
+    public void showJoke() {
+        this.activeButtonJoke(false);
+        JokeLib libJoke = new JokeLib();
+        EndpointsAsyncTask asyncTask = new EndpointsAsyncTask();
+        progressBar.setVisibility(View.VISIBLE);
+        asyncTask.setListenerResponse(new EndpointsAsyncTask.ListenerResponse() {
+            @Override
+            public void getDownloadResponse(String mError, String mResponse) {
+                activeButtonJoke(true);
+                progressBar.setVisibility(View.GONE);
+                Intent mIntent = new Intent(getActivity(), ShowJokeActivity.class);
+                mIntent.putExtra(ShowJokeActivity.PARAM_TEXT, mResponse);
+                startActivity(mIntent);
+            }
+        });
+        asyncTask.execute(libJoke.getJoke());
+    }
+
+    public void tellJoke() {
+
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            showJoke();
+        }
+    }
+
+    /**Anuncios intersticiales**/
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(getString(R.string.test_device))//AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    @Override
+    public void onClick(View v) {
+        tellJoke();
+    }
 }
+
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,102 +1,16 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
-
-import com.example.JokeLib;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
-import com.tincio.example.jokeandroidlib.ShowJokeActivity;
-
-import com.udacity.gradle.builditbigger.service.EndpointsAsyncTask;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
-    ProgressBar progressBar;
-    InterstitialAd mInterstitialAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progressBar = (ProgressBar)findViewById(R.id.progressBar);
-
-        //add anuncion intersticiales
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
-        requestNewInterstitial();
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-
-                showJoke();
-            }
-        });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    //obtener jokes
-    public void showJoke() {
-
-        JokeLib libJoke = new JokeLib();
-        EndpointsAsyncTask asyncTask = new EndpointsAsyncTask();
-        progressBar.setVisibility(View.VISIBLE);
-        asyncTask.setListenerResponse(new EndpointsAsyncTask.ListenerResponse() {
-            @Override
-            public void getDownloadResponse(String mError, String mResponse) {
-                progressBar.setVisibility(View.GONE);
-                Intent mIntent = new Intent(MainActivity.this, ShowJokeActivity.class);
-                mIntent.putExtra(ShowJokeActivity.PARAM_TEXT, mResponse);
-                startActivity(mIntent);
-            }
-        });
-        asyncTask.execute(libJoke.getJoke());
-    }
-    //obtener jokes
-    public void tellJoke(View view) {
-
-        if (mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
-        } else {
-            showJoke();
-        }
-    }
-
-    /**Anuncios intersticiales**/
-    private void requestNewInterstitial() {
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice("B1B30FBDDBEF32919AF6C5099897C2FA")//AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-
-        mInterstitialAd.loadAd(adRequest);
     }
 
 }
